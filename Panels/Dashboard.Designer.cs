479d0499153f7eb646bf8c3aceb5fd48bb02b90e
OuBnOu7 <omar7benouahi@gmail.com>
2023-05-11 15:29:18 +0000

    test

--- Panels/Dashboard.Designer.cs
+++ Panels/Dashboard.Designer.cs
@@ -28,14 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Syncfusion.Windows.Forms.Chart.ChartSeries chartSeries1 = new Syncfusion.Windows.Forms.Chart.ChartSeries();
+            Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo chartCustomShapeInfo1 = new Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo();
+            Syncfusion.Windows.Forms.Chart.ChartLineInfo chartLineInfo1 = new Syncfusion.Windows.Forms.Chart.ChartLineInfo();
+            Syncfusion.Windows.Forms.Chart.ChartSeries chartSeries2 = new Syncfusion.Windows.Forms.Chart.ChartSeries();
+            Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo chartCustomShapeInfo2 = new Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo();
+            this.pieChart = new Syncfusion.Windows.Forms.Chart.ChartControl();
+            this.chartControl2 = new Syncfusion.Windows.Forms.Chart.ChartControl();
             this.SuspendLayout();
+            // 
+            // pieChart
+            // 
+            this.pieChart.BackInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74))))));
+            this.pieChart.ChartArea.BackInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.Transparent);
+            this.pieChart.ChartArea.CursorLocation = new System.Drawing.Point(0, 0);
+            this.pieChart.ChartArea.CursorReDraw = false;
+            this.pieChart.DataSourceName = "[none]";
+            this.pieChart.IsWindowLess = false;
+            // 
+            // 
+            // 
+            this.pieChart.Legend.Location = new System.Drawing.Point(713, 81);
+            this.pieChart.Localize = null;
+            this.pieChart.Location = new System.Drawing.Point(12, 12);
+            this.pieChart.Name = "pieChart";
+            this.pieChart.PrimaryXAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.pieChart.PrimaryXAxis.Margin = true;
+            this.pieChart.PrimaryXAxis.TitleColor = System.Drawing.SystemColors.ControlText;
+            this.pieChart.PrimaryYAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.pieChart.PrimaryYAxis.Margin = true;
+            this.pieChart.PrimaryYAxis.TitleColor = System.Drawing.SystemColors.ControlText;
+            chartSeries1.FancyToolTip.ResizeInsideSymbol = true;
+            chartSeries1.Name = "Default0";
+            chartSeries1.Resolution = 0D;
+            chartSeries1.StackingGroup = "Default Group";
+            chartSeries1.Style.AltTagFormat = "";
+            chartSeries1.Style.DrawTextShape = false;
+            chartLineInfo1.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+            chartLineInfo1.Color = System.Drawing.SystemColors.ControlText;
+            chartLineInfo1.DashPattern = null;
+            chartLineInfo1.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartLineInfo1.Width = 1F;
+            chartCustomShapeInfo1.Border = chartLineInfo1;
+            chartCustomShapeInfo1.Color = System.Drawing.SystemColors.HighlightText;
+            chartCustomShapeInfo1.Type = Syncfusion.Windows.Forms.Chart.ChartCustomShape.Square;
+            chartSeries1.Style.TextShape = chartCustomShapeInfo1;
+            chartSeries1.Text = "Default0";
+            chartSeries1.Type = Syncfusion.Windows.Forms.Chart.ChartSeriesType.Pie;
+            chartSeries2.FancyToolTip.ResizeInsideSymbol = true;
+            chartSeries2.Name = "Default1";
+            chartSeries2.Resolution = 0D;
+            chartSeries2.StackingGroup = "Default Group";
+            chartSeries2.Style.AltTagFormat = "";
+            chartSeries2.Style.DrawTextShape = false;
+            chartCustomShapeInfo2.Border = chartLineInfo1;
+            chartCustomShapeInfo2.Color = System.Drawing.SystemColors.HighlightText;
+            chartCustomShapeInfo2.Type = Syncfusion.Windows.Forms.Chart.ChartCustomShape.Square;
+            chartSeries2.Style.TextShape = chartCustomShapeInfo2;
+            chartSeries2.Text = "Default1";
+            chartSeries2.Type = Syncfusion.Windows.Forms.Chart.ChartSeriesType.Pie;
+            this.pieChart.Series.Add(chartSeries1);
+            this.pieChart.Series.Add(chartSeries2);
+            this.pieChart.Size = new System.Drawing.Size(795, 428);
+            this.pieChart.TabIndex = 0;
+            this.pieChart.Text = "chartControl1";
+            // 
+            // 
+            // 
+            this.pieChart.Title.Name = "Default";
+            this.pieChart.Titles.Add(this.pieChart.Title);
+            this.pieChart.VisualTheme = "";
+            // 
+            // chartControl2
+            // 
+            this.chartControl2.BackInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74))))));
+            this.chartControl2.ChartArea.CursorLocation = new System.Drawing.Point(0, 0);
+            this.chartControl2.ChartArea.CursorReDraw = false;
+            this.chartControl2.DataSourceName = "[none]";
+            this.chartControl2.IsWindowLess = false;
+            // 
+            // 
+            // 
+            this.chartControl2.Legend.Location = new System.Drawing.Point(557, 81);
+            this.chartControl2.Localize = null;
+            this.chartControl2.Location = new System.Drawing.Point(813, 12);
+            this.chartControl2.Name = "chartControl2";
+            this.chartControl2.PrimaryXAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl2.PrimaryXAxis.Margin = true;
+            this.chartControl2.PrimaryYAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl2.PrimaryYAxis.Margin = true;
+            this.chartControl2.Size = new System.Drawing.Size(678, 428);
+            this.chartControl2.TabIndex = 1;
+            this.chartControl2.Text = "chartControl2";
+            // 
+            // 
+            // 
+            this.chartControl2.Title.Name = "Default";
+            this.chartControl2.Titles.Add(this.chartControl2.Title);
+            this.chartControl2.VisualTheme = "";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
             this.ClientSize = new System.Drawing.Size(1503, 736);
             this.ControlBox = false;
+            this.Controls.Add(this.chartControl2);
+            this.Controls.Add(this.pieChart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Dashboard";
@@ -46,5 +147,8 @@
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Chart.ChartControl pieChart;
+        private Syncfusion.Windows.Forms.Chart.ChartControl chartControl2;
     }
 }
