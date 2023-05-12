35ca9dd372e695f35d4a0dea92fcc88e1400b61c
Hayrik31 <haytambarik31@gmail.com>
2023-05-12 01:06:04 +0100

    adding dashboards

--- Panels/Dashboard.Designer.cs
+++ Panels/Dashboard.Designer.cs
@@ -28,24 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartExpense = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartIncome = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartIncome_category = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.IncomeLine = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartExpense)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncome_category)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IncomeLine)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chartExpense
+            // 
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Name = "ChartArea1";
+            this.chartExpense.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartExpense.Legends.Add(legend1);
+            this.chartExpense.Location = new System.Drawing.Point(12, 12);
+            this.chartExpense.Name = "chartExpense";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series1.Legend = "Legend1";
+            series1.Name = "Expenses";
+            this.chartExpense.Series.Add(series1);
+            this.chartExpense.Size = new System.Drawing.Size(269, 300);
+            this.chartExpense.TabIndex = 0;
+            this.chartExpense.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Exepense Tracker";
+            this.chartExpense.Titles.Add(title1);
+            // 
+            // chartIncome
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartIncome.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartIncome.Legends.Add(legend2);
+            this.chartIncome.Location = new System.Drawing.Point(12, 318);
+            this.chartIncome.Name = "chartIncome";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartIncome.Series.Add(series2);
+            this.chartIncome.Size = new System.Drawing.Size(1004, 300);
+            this.chartIncome.TabIndex = 1;
+            this.chartIncome.Text = "chart1";
+            // 
+            // chartIncome_category
+            // 
+            chartArea3.Area3DStyle.Enable3D = true;
+            chartArea3.Name = "ChartArea1";
+            this.chartIncome_category.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartIncome_category.Legends.Add(legend3);
+            this.chartIncome_category.Location = new System.Drawing.Point(302, 12);
+            this.chartIncome_category.Name = "chartIncome_category";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series3.Legend = "Legend1";
+            series3.Name = "Expenses";
+            this.chartIncome_category.Series.Add(series3);
+            this.chartIncome_category.Size = new System.Drawing.Size(275, 300);
+            this.chartIncome_category.TabIndex = 2;
+            this.chartIncome_category.Text = "chart1";
+            title2.Name = "Title1";
+            title2.Text = "Exepense Tracker";
+            this.chartIncome_category.Titles.Add(title2);
+            // 
+            // IncomeLine
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.IncomeLine.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.IncomeLine.Legends.Add(legend4);
+            this.IncomeLine.Location = new System.Drawing.Point(593, 12);
+            this.IncomeLine.Name = "IncomeLine";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.IncomeLine.Series.Add(series4);
+            this.IncomeLine.Size = new System.Drawing.Size(423, 300);
+            this.IncomeLine.TabIndex = 3;
+            this.IncomeLine.Text = "chart1";
             // 
             // Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
-            this.ClientSize = new System.Drawing.Size(1617, 863);
+            this.ClientSize = new System.Drawing.Size(1028, 627);
             this.ControlBox = false;
+            this.Controls.Add(this.IncomeLine);
+            this.Controls.Add(this.chartIncome_category);
+            this.Controls.Add(this.chartIncome);
+            this.Controls.Add(this.chartExpense);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Dashboard";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            ((System.ComponentModel.ISupportInitialize)(this.chartExpense)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartIncome_category)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IncomeLine)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartExpense;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartIncome;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartIncome_category;
+        private System.Windows.Forms.DataVisualization.Charting.Chart IncomeLine;
     }
 }
--- Panels/Dashboard.cs
+++ Panels/Dashboard.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace BudgetManagement.Panels
 {
@@ -15,6 +17,212 @@ namespace BudgetManagement.Panels
         public Dashboard()
         {
             InitializeComponent();
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\source\repos\BudgetManagement\Database1.mdf;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                // Ouvrir la connexion à la base de données
+                connection.Open();
+
+                // Requête SQL pour récupérer les données de votre table expense
+                string query = "SELECT Category, SUM(Amount) as TotalAmount FROM expense GROUP BY Category";
+
+                // Créer une commande pour exécuter la requête SQL
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Créer un adaptateur de données pour exécuter la commande et remplir un DataSet
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Lier les données à votre chartExpense
+                chartExpense.DataSource = dataTable;
+                chartExpense.Series[0].XValueMember = "Category";
+                chartExpense.Series[0].YValueMembers = "TotalAmount";
+
+                // Configurer le type de chart
+                chartExpense.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+                chartExpense.Series[0].IsValueShownAsLabel = true;
+                chartExpense.Legends[0].Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                // Gérer les erreurs de connexion
+                MessageBox.Show("Une erreur est survenue lors de la connexion à la base de données : " + ex.Message);
+            }
+            finally
+            {
+                // Fermer la connexion à la base de données
+                connection.Close();
+            }
+            try
+            {
+                // Open the database connection
+                connection.Open();
+
+                // Create SQL query to retrieve data from income and expense tables for the last month
+                string query = "SELECT Date, SUM(Amount) AS TotalAmount, 'Income' AS Category FROM income WHERE Date >= DATEADD(month, -1, GETDATE()) GROUP BY Date " +
+                               "UNION ALL " +
+                               "SELECT Date, SUM(Amount) AS TotalAmount, 'Expense' AS Category FROM expense WHERE Date >= DATEADD(month, -1, GETDATE()) GROUP BY Date";
+
+                // Create a command to execute the SQL query
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Create a data adapter to execute the command and fill a data set
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+
+                // Clear any existing series from the chart
+                chartIncome.Series.Clear();
+
+                // Add a series for income and expense data
+                chartIncome.Series.Add("Income");
+                chartIncome.Series.Add("Expense");
+
+                // Set the chart type to column
+                chartIncome.Series["Income"].ChartType = SeriesChartType.Column;
+                chartIncome.Series["Expense"].ChartType = SeriesChartType.Column;
+
+                // Set the colors of the columns
+                chartIncome.Series["Income"].Color = Color.Blue;
+                chartIncome.Series["Expense"].Color = Color.Red;
+
+                // Bind the data to the chart
+                chartIncome.DataSource = dataSet.Tables[0];
+
+                // Set the X and Y value members for each series
+                chartIncome.Series["Income"].XValueMember = "Date";
+                chartIncome.Series["Income"].YValueMembers = "TotalAmount";
+                chartIncome.Series["Expense"].XValueMember = "Date";
+                chartIncome.Series["Expense"].YValueMembers = "TotalAmount";
+
+                // Add a legend to distinguish the series
+                chartIncome.Legends.Add(new Legend("Legend"));
+                chartIncome.Series["Income"].Legend = "Legend";
+                chartIncome.Series["Expense"].Legend = "Legend";
+
+                // Set the axis labels and formatting
+                chartIncome.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+                chartIncome.ChartAreas[0].AxisX.Interval = 1;
+                chartIncome.ChartAreas[0].AxisX.LabelStyle.Format = "dd/MM/yyyy";
+
+                chartIncome.ChartAreas[0].AxisY.Minimum = 0;
+                chartIncome.ChartAreas[0].AxisY.LabelStyle.Format = "{0:C0}";
+            }
+
+            catch (Exception ex)
+            {
+                // gérer les erreurs de connexion
+                MessageBox.Show("Une erreur est survenue lors de la connexion à la base de données : " + ex.Message);
+            }
+            finally
+            {
+                // Close the database connection
+                connection.Close();
+            }
+            try
+            {
+                // Open the database connection
+                connection.Open();
+
+                // Create SQL query to retrieve data from income table
+                string query = "SELECT Category, SUM(Amount) AS TotalAmount FROM income GROUP BY Category";
+
+                // Create a command to execute the SQL query
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // Create a data adapter to execute the command and fill a data set
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+
+                // Clear any existing series from the chart
+                chartIncome_category.Series.Clear();
+
+                // Add a series for income category data
+                chartIncome_category.Series.Add("Categories");
+
+                // Set the chart type to donut
+                chartIncome_category.Series["Categories"].ChartType = SeriesChartType.Doughnut;
+
+                // Bind the data to the chart
+                chartIncome_category.DataSource = dataSet.Tables[0];
+
+                // Set the X and Y value members for the series
+                chartIncome_category.Series["Categories"].XValueMember = "Category";
+                chartIncome_category.Series["Categories"].YValueMembers = "TotalAmount";
+
+                // Set the label and legend text
+                chartIncome_category.Series["Categories"].Label = "#PERCENT{P0}";
+                chartIncome_category.Series["Categories"].LegendText = "#VALX";
+
+                // Add a legend to the chart
+                chartIncome_category.Legends.Add(new Legend("Legend"));
+                chartIncome_category.Series["Categories"].Legend = "Legend";
+
+                // Set the color palette for the chart
+                chartIncome_category.Palette = ChartColorPalette.BrightPastel;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Close the database connection
+                connection.Close();
+            }
+            try{
+using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT Date, SUM(Amount) AS TotalAmount FROM income GROUP BY Date", sqlConnection))
+                {
+                    using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand))
+                    {
+                        DataTable dataTable = new DataTable();
+                        sqlDataAdapter.Fill(dataTable);
+
+                        // Show message box with query result
+                        string message = "";
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            message += row["Date"].ToString() + " - " + row["TotalAmount"].ToString() + "\n";
+                        }
+                        MessageBox.Show(message);
+
+                        // Plot chart
+                        IncomeLine.Series.Clear();
+                        IncomeLine.Series.Add("Income");
+                        IncomeLine.Series["Income"].ChartType = SeriesChartType.Line;
+                        IncomeLine.Series["Income"].Color = Color.Blue;
+                        IncomeLine.DataSource = dataTable;
+                        IncomeLine.Series["Income"].IsXValueIndexed = false;
+                        IncomeLine.Series["Income"].XValueMember = "Date";
+                        IncomeLine.Series["Income"].YValueMembers = "TotalAmount";
+                        IncomeLine.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+                        IncomeLine.ChartAreas[0].AxisX.Interval = 1;
+                        IncomeLine.ChartAreas[0].AxisX.LabelStyle.Format = "dd/MM/yyyy";
+                        IncomeLine.ChartAreas[0].AxisY.Minimum = 0;
+                        IncomeLine.ChartAreas[0].AxisY.LabelStyle.Format = "{0:C0}";
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors that occur
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Close the database connection
+                connection.Close();
+            }
         }
     }
 }
